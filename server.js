99fb168434622c59bd4247474476a158db41facd
aryan basu <aryanbasu005@gmail.com>
2021-08-20 00:25:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,19 +18,20 @@ app.use(express.json())
 
 app.get('/', (req, res) => res.send("Working...."))
 app.get('/join', (req, res) => newMeeting(req, res));
+  
 const users = [];
 
 // Join user to chat
 function userJoin(id, username, roomId) {
   const user = { id, username, roomId };
-
-  users.push(user);
+ 
 
   return user;
 }
 function getRoomUsers(roomId) {
     const abc=users.filter(user => user.roomId === roomId);
     //console.log(abc[0].username);
+    //console.log(abc.length);
     return abc;
   }
   
@@ -42,13 +43,17 @@ function userLeave(id) {
       return users.splice(index, 1)[0];
     }
   }
-io.on("connection", (socket) => {
-
- socket.on('joinRoom', ({ username, roomId }) => {
+io.on("connection", socket => {
+   
+    socket.on('joinRoom', ({ username, roomId }) => {
         const user = userJoin(socket.id, username, roomId);
         //console.log(user);
-        users.push(user);
-        console.log(user.roomId);
+        const index1 = users.findIndex(user => user.id === socket.id);
+        //console.log(index1);
+        if(index1===-1)
+       users.push(user);
+        
+      //  console.log(user.roomId);
         socket.join(user.roomId);
          // Send users and room info
          //socket.broadcast.to(user.roomId).emit('roomUsers', {
@@ -57,13 +62,23 @@ io.on("connection", (socket) => {
         users: getRoomUsers(user.roomId)
       });
     });
-    socket.on("join-room", (roomId, userId) => {
-
-        socket.join(roomId);
-        socket.broadcast.to(roomId).emit('user-connected', userId);
-
+     socket.on('msg', function(data){
+        // server side data fetched 
+        console.log(data);
+        io.sockets.emit('newmsg', data);
+     });
+   /* socket.on("join-room", (roomId, userId) => {
+       // console.log(roomId);
+        //const user=userId;
+        //console.log(user);
+        //users.push(user);
+        //io.sockets.emit('all user',users);
+        //socket.join(roomId);
+   socket.broadcast.to(roomId).emit('user-connected', userId); */ 
+        
+  
         socket.on('disconnect', () => {
-              const user = userLeave(socket.id);
+            const user = userLeave(socket.id);
             if(user)
             {
                 io.to(user.room).emit('roomUsers', {
@@ -71,18 +86,16 @@ io.on("connection", (socket) => {
                     users: getRoomUsers(user.room)
                   });
             }
-            socket.broadcast.to(roomId).emit('user-disconnected', userId)
+           // socket.broadcast.to(roomId).emit('user-disconnected', userId)
+            //console.log(io.sockets.clients().length);
         })
-        socket.on('msg', function (data) {
-            // server side data fetched 
-            console.log(data);
-            io.to(roomId).emit('newmsg', data);
-        })
+
         /*socket.on("message", (message) => {
             io.to(roomId).emit("createMessage", message, userId);
         });  */
+      //console.log(clients);
     })
-})
+
 
 server.listen(process.env.PORT || 5000, () => {
     console.log(process.env.PORT)
