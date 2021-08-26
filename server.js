3b01b68dc3d759a50cde1190f2d25cf2683d319d
Aryan basu <aryanbasu005@gmail.com>
2021-08-26 19:46:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ const app = express();
 const server = http.createServer(app);
 const io = require("socket.io")(server, { cors: { origin: '*' } });
 const { ExpressPeerServer } = require("peer");
+const firebase=require("firebase");
+
+
+const config = {
+    apiKey: "AIzaSyAVihggwV-iWKJDozK9eBa18N5tKeJqecg",
+    authDomain: "login-d970e.firebaseapp.com",
+    databaseURL: "https://login-d970e-default-rtdb.firebaseio.com",
+    projectId: "login-d970e",
+    storageBucket: "login-d970e.appspot.com",
+    messagingSenderId: "499716605752",
+    appId: "1:499716605752:web:37bb19df341143e8da63cd",
+    measurementId: "G-MK64MTNLDX"
+};
+firebase.initializeApp(config);
+ const auth = firebase.auth();
+ const firestore = firebase.firestore();
 
 const newMeeting = require("./routes/newMeeting");
 
@@ -67,13 +83,20 @@ io.on("connection", socket => {
         console.log(data);
         io.sockets.emit('newmsg', data);
      });
-    socket.on("join-room", (roomId, userId) => {
+    socket.on("join-room", (roomId, userId,displayname) => {
        // console.log(roomId);
         //const user=userId;
         //console.log(user);
         //users.push(user);
         //io.sockets.emit('all user',users);
         //socket.join(roomId);
+        const roomRef=firebase.firestore().collection(`${roomId}`).doc(`${userId}`);
+        roomRef.set({
+                        
+                            username: displayname,
+                            id:userId
+                            
+                          });
    socket.broadcast.to(roomId).emit('user-connected', userId); 
         
   
